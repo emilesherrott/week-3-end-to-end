638d0c42ef6c7051afa54f71e9bf06dd4223d996
“emilesherrott” <emilesherrott@gmail.com>
2024-07-26 11:20:22 +0100

    Add Index Route

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require("express")
 const cors = require("cors")
 
 const logger = require("./logger")
+const countryRouter = require("./routers/countries")
 
 const app = express()
 app.use(cors())
 app.use(logger)
+
+app.use("/countries", countryRouter)
 
 module.exports = app
